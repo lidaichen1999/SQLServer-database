e01a0604fee593f38d19e088653e2e1d2b7ddf3e
DaichenLi <89897801+lidaichen1999@users.noreply.github.com>
2023-09-15 00:02:11 +0200

    Update SupplierDao.java

--- java源码/cn/linyer/dao/SupplierDao.java
+++ java源码/cn/linyer/dao/SupplierDao.java
@@ -11,7 +11,7 @@ import cn.linyer.entity.Supplier;
 import cn.linyer.util.BaseDao;
 
 /**
- * @author Linyer(韩啸翔)
+ * @author Linyer(ldc)
  * 供应商数据库操作
  */
 public class SupplierDao extends BaseDao {
